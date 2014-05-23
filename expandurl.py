d1c9ad640e4b336ee3f5a22706bcbfc6f546f3fd
Statnnov <statnnov@gmail.com>
2014-05-23 16:36:26 +0400

    Implement all api

--- expandurl.py
+++ expandurl.py
@@ -32,14 +32,29 @@ def get_url(argv):
             url = arg
     return url
 
+
 if __name__ == "__main__":
     short_url = get_url(sys.argv[1:])
     api_url = expand_api_url + short_url
     res_json = urllib2.urlopen(api_url).read()
     result = json.loads(res_json)
     if result["status"] == "OK":
-        print result['end_url']
+        if result["redirects"] == 0:
+            print "{0} is already a full form or url".format(short_url)
+        elif result["redirects"] == 1:
+            print "Your url {0} refers to {1}".format(short_url, result['end_url'])
+        else:
+            length = result["redirects"] - 1  # 2 redirects mean there is 3 url in "urls" and we don't need
+                                              # the first and the last ones
+            string_to_format = "\n  * ".join(["{" + str(n) + "}" for n in range(length)])
+            redirections = "  * " + string_to_format.format(*result["urls"][1:-1])
+            print "Your url {0} refers to {1} through {2} jumps (redirections)\
+            which are: \n{3}:".format(short_url, result['end_url'], result["redirects"], redirections)
+    elif result["status"] == "TooManyRedirects":
+        print "There is too may redirects (more than 10) so target url is not safe to reach"
     elif result["status"] == "InvalidURL":
         print "URL is invalid, check it and try again"
+    elif result["status"] == "InternalError":
+        print "Internal Error occurred, sorry, no more useful information, may be try later? "
     else:
         print "Something went wrong, url cannot be revealed"
