7d6253c96a94902e2d7815c26148b28f160e2918
Statnnov <statnnov@gmail.com>
2014-05-23 13:39:23 +0400

    Initial commit

---
+++ expandurl.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json, sys, getopt, urllib2
+
+expand_api_url = "http://expandurl.appspot.com/expand?url="
+
+def get_url(argv):
+    url = ""
+    try:
+        opts, args = getopt.getopt(argv, "hu:", ["help", "url="])
+    except getopt.GetoptError:
+        print "usage: expandurl.py -u <URL>, type -h or --help for more information."
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            help_message = "Reveal full url form its shorten form made by t.com etc.\n\n\
+options:\n\
+ -h, --help     Display this message\n\
+ -u -url <URL>  Show target url"
+            print help_message
+            sys.exit()
+        elif opt in ("-u", "--url"):
+            url = arg
+    return url
+
+if __name__ == "__main__":
+    short_url = get_url(sys.argv[1:])
+    api_url = expand_api_url + short_url
+    res_json = urllib2.urlopen(api_url).read()
+    result = json.loads(res_json)
+    if result["status"] == "OK":
+        print result['end_url']
+    else:
+        print "Something went wrong, url cannot be revealed"
+
+
